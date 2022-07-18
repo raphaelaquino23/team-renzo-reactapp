b6f1efeed78084871bf653122397e681e40860c0
renzoancajas24 <108566575+renzo-ancajas@users.noreply.github.com>
2022-07-18 14:33:48 +0800

    Edited app text!

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EDIT but in silence <code>src/App.tsx</code> and save to reload.
+          EDIT but in silence HELLO (RENZO EDITED THIS)<code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
